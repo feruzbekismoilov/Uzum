e4e1731dba133b440897bf5c7e725007f80864df
feruzbekismoilov <ismoilovferuz0858@gmail.com>
2023-04-12 12:28:37 +0500

    add products

--- js/main.js
+++ js/main.js
@@ -1,6 +1,54 @@
+
 var swiper = new Swiper(".mySwiper", {
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
   });
+
+  
+  const elProductsList = document.querySelector(".popular__list");
+  const elProductsTemplate = document.querySelector(".product-template").content;
+  const ProductsFragment = new DocumentFragment();
+  function renderProducts(_products) {
+    elProductsList.innerHTML = null;
+    _products.forEach(product =>{
+      let CloneTemplate = elProductsTemplate.cloneNode(true);
+      CloneTemplate.querySelector(".popular__item__img").src = product.img;
+      CloneTemplate.querySelector(".popular__item__desc").textContent = product.name;
+      CloneTemplate.querySelector(".popular__item__monthly-payment").textContent = `${product.oylik_sum}  сум/мес`;
+      CloneTemplate.querySelector(".popular__item__old-price").textContent = `${product.old_sum} сум`;
+      CloneTemplate.querySelector(".popular__item__new-price").textContent = `${product.new_sum} сум`;
+   
+     ProductsFragment.appendChild(CloneTemplate);
+   })
+   
+   elProductsList.appendChild(ProductsFragment);
+  }
+  renderProducts(products);
+
+
+const elSearchForm = document.querySelector(".header__middle__form");
+const elSearchInput = elSearchForm.querySelector(".header__middle__form__input");
+
+elSearchForm.addEventListener("submit", function(evt){
+  evt.preventDefault();
+  const inputValue = elSearchInput.value.trim();
+  const SearchQuery = new RegExp(inputValue, "gi");
+  const searchProduct = showSearchResult(SearchQuery);
+
+  if(searchProduct.length>0){
+    renderProducts(searchProduct);
+  }else{
+    elProductsList.innerHTML = `<div class = "text-white display-3" >Not found product!</div>`
+  }
+
+})
+
+
+function showSearchResult (regexp){
+  const filteredProducts = products.filter((product) => 
+  String(product.name).match(regexp) 
+  );
+  return filteredProducts;
+}
---
+++ js/products.js
@@ -0,0 +1,65 @@
+const products = [
+    {
+        "id": 1,
+        "img": "./images/tide.jpg",
+        "name": "Стиральный порошок автомат Tide альпийская свежесть, 3 кг",
+        "oylik_sum":8800,
+        "old_sum":93000,
+        "new_sum":74000
+    },
+    {
+        "id": 2,
+        "img": "./images/esty.jpg",
+        "name": "Туалетная бумага Esty, 6 шт",
+        "oylik_sum":1560,
+        "old_sum":16000,
+        "new_sum":13000
+    },
+    {
+        "id": 3,
+        "img": "./images/pegas.jpg",
+        "name": "Жидкое мыло-крем Pegas, 1 л",
+        "oylik_sum":1680,
+        "old_sum":25000,
+        "new_sum":14000
+    },
+    {
+        "id": 4,
+        "img": "./images/skovorotka.jpg",
+        "name": "Сковорода Kukmara со съемной ручкой, АП линия 'Granit ultra' (Blue)",
+        "oylik_sum":20400,
+        "old_sum":227000,
+        "new_sum":170000
+    },
+    {
+        "id": 5,
+        "img": "./images/esty120.jpg",
+        "name": "Детские влажные салфетки Esty Premium, 120 шт",
+        "oylik_sum":1440,
+        "old_sum":18000,
+        "new_sum":12000
+    },
+    {
+        "id": 6,
+        "img": "./images/chistki.jpg",
+        "name": "Набор инструментов для чистки лица удаления черных точек 4 предмета, угрей от прыщей, акне",
+        "oylik_sum":157560,
+        "old_sum":1357000,
+        "new_sum":1313000
+    },
+    {
+        "id": 7,
+        "img": "./images/esty-bumag.jpg",
+        "name": "Туалетная бумага Esty, 8 шт",
+        "oylik_sum":32400,
+        "new_sum":270000
+    },
+    {
+        "id": 8,
+        "img": "./images/power-bank.jpg",
+        "name": "Внешний аккумулятор Xiaomi Mi 18W Fast Charge Power Bank 3 10000, чёрный",
+        "oylik_sum":2040,
+        "old_sum":25000,
+        "new_sum":17000
+    }
+];
